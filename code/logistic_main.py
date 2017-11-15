345811b479605ee02522b3bf6a1697214ee4f722
Jayant <jayantjnp@gmail.com>
2017-11-15 14:01:33 -0500

    data loader

--- code/logistic_main.py
+++ code/logistic_main.py
@@ -1,6 +1,9 @@
 #########################################################################################
 #Description:
-#
+# Read the traning data and perform the calculations
 #
 #########################################################################################
-
+import numpy as np
+from data_loader import *
+(trains_image,train_images_label) =read_gz();
+numpy.savez("testfile.npz", test_array_1=test_array_1, test_array_2=test_array_2)
